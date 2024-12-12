fbfd933ba6749ef0e6ad0e1cfab9485abd1d7c5f
Dinis Pereira <dinisazpereira@gmail.com>
2024-12-12 08:28:12 +0000

    Novo update: Botao de fazer reserva a funcionar

--- src/pages/HotelDetails/index.tsx
+++ src/pages/HotelDetails/index.tsx
@@ -11,20 +11,29 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateField } from '@mui/x-date-pickers/DateField';
 import Button from '@mui/material/Button';
 
+
+
+
 export const HotelDetails: React.FC = () => {
   const { id } = useParams<{ id: string }>(); // Vai buscar o ID que tem no URL
   const [hotel, setHotel] = useState<Hotel | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [roomId, setRoomId] = useState("");
-  const [startDate, setStartDate] = React.useState<Dayjs | null>(dayjs('2022-04-17'));
-  const [endDate, setEndDate] = React.useState<Dayjs | null>(dayjs('2022-04-17'));
+  const [startDate, setStartDate] = React.useState<Dayjs | null>(dayjs('2022-08-08'));
+  const [endDate, setEndDate] = React.useState<Dayjs | null>(dayjs('2022-08-08'));
+  const formattedStartDate = startDate ? startDate.format('YYYY-MM-DD') : null; // condição se o valor for verdade formatar para o formato da api senao for verdadeiro deixar vazio
+  const formattedEndDate = endDate ? endDate.format('YYYY-MM-DD') : null; // condição se o valor for verdade formatar para o formato da api senao for verdadeiro deixar vazio
 
 
+  function formatDate(DateC: string) {
+    return dayjs(DateC).format('DD-MM-YYYY');
+  }
 
 
   useEffect(() => {
     const fetchHotelDetails = async () => {
+      console.log(formatDate("2024/12/11"));
       setError(null);
       setLoading(true);
       try {
@@ -36,6 +45,8 @@ export const HotelDetails: React.FC = () => {
         }
         const data = await response.json();
         setHotel(data.hotel); // Atualiza o estado com os detalhes do hotel
+        console.log(data.hotel);
+
       } catch (err: any) {
         setError(err.message || 'Erro desconhecido.');
       } finally {
@@ -48,20 +59,38 @@ export const HotelDetails: React.FC = () => {
 
   async function makeReservation(){
 
+    if (!roomId || !formattedStartDate || !formattedEndDate) {
+      toast.error("Por favor, preencha todos os campos antes de fazer a reserva.", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+      });
+      return;
+    }
+
     try{
 
       const requestBody = {
         roomId: roomId,
-        startDate: startDate,
-        endDate: endDate,
+        startDate: formattedStartDate,
+        endDate: formattedEndDate
 
       };
+      console.log("Enviando para a API:", requestBody);
       const response = await fetch(
         "https://api-tma-2024-production.up.railway.app/booking",
         {
           method: "POST",
           headers:{
-            "Content-Type" : "application/json"
+            "Content-Type" : "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+
           },
             body: JSON.stringify(requestBody),
             }
@@ -87,7 +116,7 @@ export const HotelDetails: React.FC = () => {
               console.log("Reserva efetuado com sucesso rei");
             } else if (response.status === 400){
               const data = await response.json(); 
-              console.error(response.json);
+              console.log(response.json);
               
               toast.error(data.massage, {
               position: "top-center",
@@ -201,7 +230,12 @@ export const HotelDetails: React.FC = () => {
                   <DateField
                       label="Inicio"
                       value={startDate}
-                      onChange={(newValue) => setStartDate(newValue)}
+                      onChange={(newValue) => {
+                        setStartDate(newValue);
+                        
+                       
+                      
+                      }}
                     />
                     <DateField
                       label="Fim"
@@ -210,7 +244,13 @@ export const HotelDetails: React.FC = () => {
                     />
                   </DemoContainer>
                 </LocalizationProvider>
-                <Button variant="contained" onClick={makeReservation}>Fazer Reserva</Button>
+                <Button variant="contained"  onClick={() =>{
+                  setRoomId(room.id);
+                  makeReservation();
+                }}
+                  
+                  
+                  >Fazer Reserva</Button>
 
                         
                   </CardContent>
